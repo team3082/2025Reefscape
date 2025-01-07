2148109b0d71feaeb9e8d7f66ea58659b3b6f138
ArcticWarmth <ArcticWarmthDev@protonmail.com>
2025-01-07 16:45:47 -0600

    Setup WPIlib for 2025

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,45 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+
+
+public class Robot extends TimedRobot {
+  @Override
+  public void robotPeriodic() {}
+
+  
+  @Override
+  public void autonomousInit() {
+    
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    
+  }
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
