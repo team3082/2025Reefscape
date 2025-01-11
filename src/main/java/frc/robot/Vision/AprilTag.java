61a10aeccf905a8c3aba1c1f635e8f78302da490
Sir-Tuc <155198681+CurtisLindahl@users.noreply.github.com>
2025-01-11 11:20:15 -0600

    add apriltag constants

---
+++ src/main/java/frc/robot/Vision/AprilTag.java
@@ -0,0 +1,17 @@
+package frc.robot.Vision;
+
+public class AprilTag {
+
+    public double x, y, z, rotZ, rotY;
+
+    public AprilTag(double x, double y, double z, double rotZ, double rotY) {
+
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.rotZ = rotZ;
+        this.rotY = rotY;
+
+    }
+
+}
