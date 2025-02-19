cd7587e6e46d144a9b782695140cec18ec09c211
Sir-Tuc <155198681+CurtisLindahl@users.noreply.github.com>
2025-02-18 22:19:26 -0600

    cool oi changes (sorry lucas i took a button away from you)

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -23,42 +23,45 @@ public class OI {
     // ------------------ Driver Controls ------------------ //
 
     // Movement
-    static final int moveX         = LogitechF310.AXIS_LEFT_X;
-    static final int moveY         = LogitechF310.AXIS_LEFT_Y;
-    static final int rotateX       = LogitechF310.AXIS_RIGHT_X;
-    // static final int rotateX       = 2;
-    static final int boost         = LogitechF310.AXIS_RIGHT_TRIGGER;
-
-    // zero is for Pigeon
-    static final int zero          = LogitechF310.BUTTON_Y;
-
-    static final int funnyButtonLeft   = LogitechF310.DPAD_LEFT;
-    static final int funnyButtonRight  = LogitechF310.DPAD_RIGHT;
-    private static boolean drivingToReef = false;
-
-    private static final int lockIn     = LogitechF310.BUTTON_X;
-    private static final int lockOut     = LogitechF310.BUTTON_B;
-
-
+    static final int moveX                       = LogitechF310.AXIS_LEFT_X;
+    static final int moveY                       = LogitechF310.AXIS_LEFT_Y;
+    static final int rotateX                     = LogitechF310.AXIS_RIGHT_X;
+    
+    static final int boost                       = LogitechF310.AXIS_RIGHT_TRIGGER;
+ 
+    // zero is for Pigeon 
+    static final int zero                        = LogitechF310.BUTTON_Y;
+ 
+    static final int funnyButtonLeft             = LogitechF310.DPAD_LEFT;
+    static final int funnyButtonRight            = LogitechF310.DPAD_RIGHT;
+ 
+    private static final int lockIn              = LogitechF310.BUTTON_X;
+    private static final int lockOut             = LogitechF310.BUTTON_B;
+ 
+    // End Effector Control 
+    static final int intake                      = LogitechF310.BUTTON_LEFT_BUMPER;
+    static final int outtake                     = LogitechF310.BUTTON_RIGHT_BUMPER;
+    static final int dealgaefy                   = LogitechF310.AXIS_LEFT_TRIGGER;
+ 
+    private static boolean drivingToReef         = false;
+    private static boolean previouslyPressedPOV  = false; // Checks if we previously pressed the dpad because getPOV() doesn't do that
+ 
+ 
     // ------------------ Operator Controls ------------------ //
-    public static Joystick operatorStick;
+    public static Joystick operatorStick; 
+ 
+    // Scoring Positions 
+    private static final int stow                = LogitechF310.BUTTON_A;
+    private static final int L2                  = LogitechF310.BUTTON_B;
+    private static final int L3                  = LogitechF310.BUTTON_X;
+    private static final int L4                  = LogitechF310.BUTTON_Y;
+    private static final int ALGAE1              = LogitechF310.DPAD_DOWN;
+    private static final int ALGAE2              = LogitechF310.DPAD_UP;
+    private static final int LET_HIM_COOK        = LogitechF310.DPAD_LEFT;
+ 
+    private static final int RightTrigger        = LogitechF310.AXIS_RIGHT_TRIGGER;
+    private static final int LeftTrigger         = LogitechF310.AXIS_LEFT_TRIGGER;
 
-    // Scoring Positions
-    private static final int stow         = LogitechF310.BUTTON_A;
-    private static final int L2           = LogitechF310.BUTTON_B;
-    private static final int L3           = LogitechF310.BUTTON_X;
-    private static final int L4           = LogitechF310.BUTTON_Y;
-    private static final int ALGAE1      = LogitechF310.DPAD_DOWN;
-    private static final int ALGAE2      = LogitechF310.DPAD_UP;
-    private static final int DEALGAEFY      = LogitechF310.DPAD_RIGHT;
-    private static final int LET_HIM_COOK   = LogitechF310.DPAD_LEFT;
-
-    private static final int RightTrigger = LogitechF310.AXIS_RIGHT_TRIGGER;
-    private static final int LeftTrigger  = LogitechF310.AXIS_LEFT_TRIGGER;
-
-    // End Effector Control
-    static final int intake       = LogitechF310.BUTTON_LEFT_BUMPER;
-    static final int outtake      = LogitechF310.BUTTON_RIGHT_BUMPER;
 
     private static ScoringPosition savedLevel = ScoringPosition.STOW;
 
@@ -112,7 +115,8 @@ public class OI {
         // SWERVE
 
         // SCORING
-        if (driverStick.getPOV() == funnyButtonLeft || driverStick.getPOV() == funnyButtonRight) {
+        if ((driverStick.getPOV() == funnyButtonLeft || driverStick.getPOV() == funnyButtonRight) && !previouslyPressedPOV) {
+            previouslyPressedPOV = true;
             drivingToReef = !drivingToReef;
             if(drivingToReef) {
                 Vector2 currentPos = SwervePosition.getPosition();
@@ -121,11 +125,21 @@ public class OI {
                 allianceStartIndex = DriverStation.getAlliance().get() == Alliance.Red ? 6 : 17;
 
                 // Find the shortest scoring position from the robot
-                double min = currentPos.sub(Constants.APRIL_TAGS[allianceStartIndex].getPosition()).mag();
+                double min;
+                if (driverStick.getPOV() == funnyButtonLeft)
+                    min = currentPos.sub(Constants.APRIL_TAGS[allianceStartIndex].getLeftPosition()).mag();
+                else 
+                    min = currentPos.sub(Constants.APRIL_TAGS[allianceStartIndex].getRightPosition()).mag();
+                
                 int minIndex = allianceStartIndex;
                 for (int i = allianceStartIndex+1; i < allianceStartIndex + 6; i++){
                     System.out.println(Constants.APRIL_TAGS[i].getPosition());
-                    Vector2 aprilPosition = Constants.APRIL_TAGS[i].getPosition();
+                    Vector2 aprilPosition;
+                    if (driverStick.getPOV() == funnyButtonLeft)
+                        aprilPosition = Constants.APRIL_TAGS[i].getLeftPosition();
+                    else 
+                        aprilPosition = Constants.APRIL_TAGS[i].getRightPosition();
+
                     if(currentPos.sub(aprilPosition).mag() < min){
                         minIndex = i;
                         min = currentPos.sub(aprilPosition).mag();
@@ -140,7 +154,15 @@ public class OI {
                 SwervePID.setDestRot(Constants.APRIL_TAGS[minIndex].getRotationZ() + (DriverStation.getAlliance().get() == Alliance.Blue ? -1 : 1) * Math.PI/2);
 
             }
+        } else {
+            previouslyPressedPOV = false;
         }
+
+        /*-End Effector-------------------------------------------------------------------------------------------*/
+        if (driverStick.getRawButton(intake)) ScoringManager.endEffector.setIntakeState(IntakeState.INTAKE_PIECE);
+        else if (driverStick.getRawButton(outtake)) ScoringManager.endEffector.setIntakeState(IntakeState.DROP_PIECE);
+        else if (driverStick.getRawAxis(dealgaefy) > 0.7)  ScoringManager.endEffector.setIntakeState(IntakeState.DEALGAE);
+        else ScoringManager.endEffector.setIntakeState(IntakeState.HOLD_PIECE);
 
         /*--------------------------------------------------------------------------------------------------------*/
         // SWERVE
@@ -177,12 +199,6 @@ public class OI {
         } else if (operatorStick.getRawAxis(LeftTrigger)>0.7){
             ScoringManager.setPickingRightCoral(false);
         }
-        
-        /*-End Effector-------------------------------------------------------------------------------------------*/
-        if (driverStick.getRawButton(intake)) ScoringManager.endEffector.setIntakeState(IntakeState.INTAKE_PIECE);
-        else if (driverStick.getRawButton(outtake)) ScoringManager.endEffector.setIntakeState(IntakeState.DROP_PIECE);
-        else if (operatorStick.getPOV() == DEALGAEFY)  ScoringManager.endEffector.setIntakeState(IntakeState.DEALGAE);
-        else ScoringManager.endEffector.setIntakeState(IntakeState.HOLD_PIECE);
         
     }
 
